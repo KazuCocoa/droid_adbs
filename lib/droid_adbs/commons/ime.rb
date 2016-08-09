9795b5d2d830af30de1aa513e021bb0dffae3277
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-08-09 22:24:22 +0900

    add setting ime

--- lib/droid_adbs/commons/ime.rb
+++ lib/droid_adbs/commons/ime.rb
@@ -8,6 +8,14 @@ module DroidAdbs
         `#{::DroidAdbs.shell} ime list`.chomp
       end
 
+      # @param [String] ime_id ID provided by IME such as "com.google.android.inputmethod.japanese/.MozcService"
+      # @return [String] message from adb command
+      #   if it succeeded to set IME, then it returns "Input method com.google.android.inputmethod.japanese/.MozcService selected"
+      #   if it failed to set IME because of no ID, then it returns "Error: Unknown id: ime_id"
+      def set_ime(ime_id)
+        `#{::DroidAdbs.shell} ime set #{ime_id}`.chomp
+      end
+
       # @param [String] ime_lists
       # @return [Array] Array of IME ID list such as ["com.agilebits.onepassword/.filling.FillingInputMethodService",
       #   com.google.android.googlequicksearchbox/com.google.android.voicesearch.ime.VoiceInputMethodService"]
