8fd6bfff94c97fccea738df5593291809e6dc93a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-08-15 23:42:02 +0900

    add grant command

---
+++ lib/droid_adbs/commons/grant.rb
@@ -0,0 +1,22 @@
+require_relative "../../droid_adbs"
+
+module DroidAdbs
+  module Grant
+    class << self
+      # @param [String] package A name of package you would like to allow permission
+      # @param [Object] permission Permission you would like to allow
+      # @return [String] message from adb command
+      def grant(package:, permission:)
+        result = `#{::DroidAdbs.shell} pm grant #{package} #{permission}`.chomp
+
+        unless result.empty?
+          exception = "java.lang.IllegalArgumentException:"
+          error_message = result.each_line.find { |line| line.include? exception }.chomp
+          raise RuntimeError, message unless error_message.empty?
+        end
+
+        result
+      end
+    end
+  end
+end
