603f7652527764863e1bd50c9f7d8af0ab174944
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2017-10-17 23:50:50 +0900

    Kazu cocoa patch 1 (#26)
    * Update record.rb
    
    * add time limit and tear down
    
    * add a comment

--- lib/droid_adbs/commons/record.rb
+++ lib/droid_adbs/commons/record.rb
@@ -9,35 +9,51 @@ module DroidAdbs
 
       public
 
-      def initialize(device_serial: nil, bit_rate: 3000000, size: "1280x720")
+      def initialize(device_serial: nil, bit_rate: 3000000, size: "1280x720", time_limit: 180)
         @bit_rate = bit_rate
         @size = size
+        @time_limit = time_limit
 
         ::DroidAdbs.device_serial = device_serial if device_serial
         @recording = false
       end
 
-      def record(file_name:)
+      def start(file_name: "droid_adbs_video")
+        validate_recordable?
         raise "Recording on the other process" if @recording
 
         @file_name = file_name
 
         @recording = true
         @process_id = fork do
-          exec "#{::DroidAdbs.shell} screenrecord --bit-rate #{@bit_rate} --size #{@size} /sdcard/#{@file_name}.mp4"
+          exec "#{::DroidAdbs.shell} screenrecord --time-limit #{@time_limit} --bit-rate #{@bit_rate} --size #{@size} /sdcard/#{@file_name}.mp4"
         end
       end
 
-      def kill
+      def stop
+        validate_recordable?
         Process.kill(:SIGINT, @process_id)
         @recording = false
+      end
 
+      def pull(export_to: "#{Dir.pwd}/droid_adbs_video.mp4")
         # Download the video
-        system "#{::DroidAdbs.adb_serial} pull /sdcard/#{@file_name}.mp4 #{Dir.pwd}"
-        sleep 5 # To finish pull command
+        system "#{::DroidAdbs.adb_serial} pull /sdcard/#{@file_name}.mp4 #{export_to}"
+      end
 
+      def delete
         # Delete the video from the device
         system "#{::DroidAdbs.shell} rm /sdcard/#{@file_name}.mp4"
+      end
+
+      private
+
+      def validate_recordable?
+        unless ::DroidAdbs::Devices.device_build_version_sdk.to_i >= 19
+          raise "The command require over KitKat(API Level 19)"
+        end
+
+        true
       end
     end
   end
--- test/droid_adbs/commons/record_test.rb
+++ test/droid_adbs/commons/record_test.rb
@@ -3,14 +3,19 @@ require 'test_helper'
 class DroidAdbsCommonsRecordTest < Minitest::Test
   def test_record
     record = ::DroidAdbs::ScreenRecord::Recording.new
-    record.record file_name: "sample"
+    record.start
 
     sleep 10
 
-    record.kill
-    assert File.exist? "./sample.mp4"
+    record.stop
 
-    # tear down
-    File.delete "./sample.mp4"
+    # Should wait to finish exporting recorded file
+    sleep 1
+
+    record.pull
+    assert File.exist? "./droid_adbs_video.mp4"
+
+    record.delete
+    File.delete "./droid_adbs_video.mp4"
   end
 end
