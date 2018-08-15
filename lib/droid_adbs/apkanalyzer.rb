33b810faee7d4f90d5b09e8b1006164117c59afb
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-08-15 16:31:33 +0900

    define private

--- lib/droid_adbs/apkanalyzer.rb
+++ lib/droid_adbs/apkanalyzer.rb
@@ -17,7 +17,7 @@ module DroidAdbs
     #     manifest_package_id #=> "io.appium.android.apis"
     #
     def manifest_package_id
-      `apkanalyzer manifest application-id #{@apk_path}`.strip
+      `#{command_manifest} application-id #{@apk_path}`.strip
     end
 
     # @param [String] out_path A path to output the result
@@ -25,7 +25,7 @@ module DroidAdbs
     #
     #
     def manifest_print(out_path = './manifest_print.xml')
-      result = `apkanalyzer manifest print #{@apk_path}`.strip
+      result = `#{command_manifest} print #{@apk_path}`.strip
       File.write out_path, result
     end
 
@@ -36,7 +36,7 @@ module DroidAdbs
     #     manifest_version_name #=> "1.0"
     #
     def manifest_version_name
-      `apkanalyzer manifest version-name #{@apk_path}`.strip
+      `#{command_manifest} version-name #{@apk_path}`.strip
     end
 
     # @return [String] A number of version code by string
@@ -46,7 +46,7 @@ module DroidAdbs
     #     manifest_version_code #=> "1"
     #
     def manifest_version_code
-      `apkanalyzer manifest version-code #{@apk_path}`.strip
+      `#{command_manifest} version-code #{@apk_path}`.strip
     end
 
     # @return [String] A number of min sdk by string
@@ -56,7 +56,7 @@ module DroidAdbs
     #     manifest_min_sdk #=> "16"
     #
     def manifest_min_sdk
-      `apkanalyzer manifest min-sdk #{@apk_path}`.strip
+      `#{command_manifest} min-sdk #{@apk_path}`.strip
     end
 
     # @return [String] A number of target sdk by string
@@ -66,7 +66,7 @@ module DroidAdbs
     #     manifest_target_sdk #=> "22"
     #
     def manifest_target_sdk
-      `apkanalyzer manifest target-sdk #{@apk_path}`.strip
+      `#{command_manifest} target-sdk #{@apk_path}`.strip
     end
 
     # @return [[String]] A list of permissions
@@ -76,7 +76,7 @@ module DroidAdbs
     #     manifest_permissions #=> ["android.permission.INTERNET", "android.permission.SYSTEM_ALERT_WINDOW", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_PHONE_STATE"]
     #
     def manifest_permissions
-      `apkanalyzer manifest permissions #{@apk_path}`.strip.split("\n")
+      `#{command_manifest} permissions #{@apk_path}`.strip.split("\n")
     end
 
     # @return [Bool] Debuggable or not
@@ -86,7 +86,7 @@ module DroidAdbs
     #     manifest_debuggable #=> true
     #
     def manifest_debuggable
-      result = `apkanalyzer manifest debuggable #{@apk_path}`.strip
+      result = `#{command_manifest} debuggable #{@apk_path}`.strip
       return true if result == "true"
       false
     end
@@ -98,13 +98,13 @@ module DroidAdbs
     #     apk_summary #=> ["com.vodqareactnative", "1", "1.0"]
     #
     def apk_summary
-      `apkanalyzer apk summary #{@apk_path}`.strip.split("\t")
+      `#{command_apk} summary #{@apk_path}`.strip.split("\t")
     end
 
     # Prints the file size of the APK.
     # 7_414_686 (byte)
     def apk_file_size
-      `apkanalyzer apk file-size #{@apk_path}`.strip
+      `#{command_apk} file-size #{@apk_path}`.strip
     end
 
     # Prints an estimate of the download size of the APK.
@@ -114,7 +114,7 @@ module DroidAdbs
     #     apk_download_size #=> 7313000
     #
     def apk_download_size
-      `apkanalyzer apk download-size #{@apk_path}`.strip
+      `#{command_apk} download-size #{@apk_path}`.strip
     end
 
     # Prints features used by the APK.
@@ -124,12 +124,12 @@ module DroidAdbs
     #     apk_features #=> "android.hardware.faketouch implied: default feature for all apps"
     #
     def apk_features
-      puts `apkanalyzer apk features #{@apk_path}`.strip
+      puts `#{command_apk} features #{@apk_path}`.strip
     end
 
     # Compares the sizes of two APKs.
     def apk_compare_print(with)
-      puts `apkanalyzer apk features #{@apk_path} #{with}`.strip
+      puts `#{command_apk} features #{@apk_path} #{with}`.strip
     end
 
     # Lists all files in the zip.
@@ -144,29 +144,29 @@ module DroidAdbs
 
     # Prints a list of dex files in the APK
     def dex_list
-      `apkanalyzer dex list #{@apk_path}`.strip.split("\n")
+      `#{command_dex} list #{@apk_path}`.strip.split("\n")
     end
 
     # Prints number of references in dex files
     def dex_references(options = "")
-      `apkanalyzer dex references #{options} #{@apk_path}`.strip
+      `#{command_dex} references #{options} #{@apk_path}`.strip
     end
 
     # Prints the class tree from DEX.
     # P,C,M,F: indicates packages, classes methods, fields
     # x,k,r,d: indicates removed, kept, referenced and defined nodes
     def dex_packages(options = "")
-      `apkanalyzer dex packages #{options} #{@apk_path}`.strip
+      `#{command_dex} packages #{options} #{@apk_path}`.strip
     end
 
     # Prints the bytecode of a class or method in smali format
     def dex_code
-      `apkanalyzer dex code #{@apk_path}`.strip
+      `#{command_dex} code #{@apk_path}`.strip
     end
 
     # Prints a list of packages in resources table
     def resources_packages
-      `apkanalyzer resources packages #{@apk_path}`.strip
+      `#{command_resources} packages #{@apk_path}`.strip
     end
 
     # Prints a list of configurations for the specified type.
@@ -175,7 +175,7 @@ module DroidAdbs
     #                         otherwise the first defined package will be used.
     #
     def resources_configs(type, package = "")
-      `apkanalyzer resources configs --type #{type} #{package} #{@apk_path}`.strip
+      `#{command_resources} configs --type #{type} #{package} #{@apk_path}`.strip
     end
 
     # Prints the value of the resource specified by config, name, and type.
@@ -190,12 +190,31 @@ module DroidAdbs
     #     resources_value "default", "revision_hash", "string"
     #
     def resources_value(config, name, type, package = "")
-      `apkanalyzer resources value --config #{config} --name #{name} --type #{type} #{package} #{@apk_path}`.strip
+      `#{command_resources} value --config #{config} --name #{name} --type #{type} #{package} #{@apk_path}`.strip
     end
 
     # Prints the human-readable form of a binary XML file. Include the file option to specify the path to the file.
     def resources_xml(xml_file)
-      `apkanalyzer resources xml --file #{xml_file} #{@apk_path}`.strip
+      `#{command_resources} xml --file #{xml_file} #{@apk_path}`.strip
     end
+
+    private
+
+    def command_manifest
+      'apkanalyzer manifest'
+    end
+
+    def command_apk
+      'apkanalyzer apk'
+    end
+
+    def command_dex
+      'apkanalyzer dex'
+    end
+
+    def command_resources
+      'apkanalyzer resources'
+    end
+
   end # class Apkanalyzer
 end # module DroidAdbs
